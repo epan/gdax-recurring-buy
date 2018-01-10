a3c827028524f3dec225107c18d512c2870e29b1
Eric Pan <epan@users.noreply.github.com>
2018-01-10 00:56:04 -0800

    (feat) Add CONSTS file and buys every 0.3 seconds

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const formatOut = bformat({ outputMode: 'long' });
 const logger = bunyan.createLogger({ name: 'app', stream: formatOut, level: 'debug' });
 
 const config = require('./config');
+const CONSTS = require('./CONSTS');
 
-const apiURI = config.sandboxMode ? 'https://api-public.sandbox.gdax.com' : 'https://api.gdax.com'
+const apiURI = config.sandboxMode ? CONSTS.sandboxURI : CONSTS.productionURI;
 
 const authedSandboxClient = new Gdax.AuthenticatedClient(
   config.gdax.sandbox.apiKey,
@@ -16,6 +17,21 @@ const authedSandboxClient = new Gdax.AuthenticatedClient(
   apiURI,
 );
 
-authedSandboxClient.getAccounts()
+const buyParams = {
+  product_id: CONSTS.BTCUSD,
+  size: 0.01,
+  price: 100,
+};
+
+// Creates an order for 0.01 BTC at $100 USD every 0.3 seconds
+const buyBTC = () => {
+  authedSandboxClient.buy(buyParams)
   .then((data) => { logger.info(data); })
   .catch((error) => { logger.error(error); });
+
+  setTimeout(() => {
+    buyBTC();
+  }, 300);
+};
+
+buyBTC();
