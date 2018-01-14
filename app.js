e902c2edfa5307a6c51f11f12c472ece23d72393
Eric Pan <epan@users.noreply.github.com>
2018-01-13 16:45:11 -0800

    Use dotenv for variables (#3)
    Use env variables so app can be deployed

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
 const Gdax = require('gdax');
 const bformat = require('bunyan-format');
 const bunyan = require('bunyan');
 
+// Gets user-input settings from config.json
+const config = require('./config');
+
+// Formats logging to console
 const formatOut = bformat({ outputMode: 'long' });
 const logger = bunyan.createLogger({
   name: 'app',
@@ -9,19 +14,20 @@ const logger = bunyan.createLogger({
   level: 'debug',
 });
 
-const config = require('./config');
+// Sets up the delay between each order
+const { numberOfDays } = config;
+const DAY = 1000 * 60 * 60 * 24; // 1000ms * 60sec * 60min * 24hr
+const daysBetweenOrders = numberOfDays * DAY;
 
-const { sandbox, production, sandboxMode, numberOfDays } = config;
-const DAY = 1000 * 60 * 60 * 24;
-const env = sandboxMode ? sandbox : production;
-
+// Authenticates with the API
 const authedClient = new Gdax.AuthenticatedClient(
-  env.apiKey,
-  env.secret,
-  env.passPhrase,
-  env.apiURI,
+  process.env.API_KEY,
+  process.env.SECRET,
+  process.env.PASSPHRASE,
+  process.env.API_URI,
 );
 
+// Configures the order product and quantity
 const buyParams = {
   type: 'market',
   side: 'buy',
@@ -29,9 +35,17 @@ const buyParams = {
   size: 0.01, // 0.01 means 0.01 BTC which is both the minimum buy and increment
 };
 
+// Makes the orders and and repeats it
 const startRecurringOrder = () => {
-  authedClient
-    .placeOrder(buyParams)
+  authedClient.placeOrder(buyParams)
+    .then((data) => {
+      logger.info(data);
+    })
+    .catch((error) => {
+      logger.error(error);
+    });
+
+  authedClient.getFills()
     .then((data) => {
       logger.info(data);
     })
@@ -41,7 +55,8 @@ const startRecurringOrder = () => {
 
   setTimeout(() => {
     startRecurringOrder();
-  }, numberOfDays * DAY);
+  }, daysBetweenOrders);
 };
 
+// Go time
 startRecurringOrder();
